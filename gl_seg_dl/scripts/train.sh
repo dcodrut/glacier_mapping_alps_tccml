15104083f49665161232e202fef934e788c6e9e7
dcodrut <codrutandreidiaconu@gmail.com>
2023-10-02 09:17:47 +0200

    assume a single GPU is used

--- gl_seg_dl/scripts/train.sh
+++ gl_seg_dl/scripts/train.sh
@@ -8,7 +8,7 @@ cd $CWD
 # activate the conda environment
 conda activate glsegenv
 
-# train consequently each model
+# train consequently each model on a single GPU
 python ./main_train.py --setting=./configs/cv/unet_1.yaml
 python ./main_train.py --setting=./configs/cv/unet_2.yaml
 python ./main_train.py --setting=./configs/cv/unet_3.yaml
@@ -16,7 +16,7 @@ python ./main_train.py --setting=./configs/cv/unet_4.yaml
 python ./main_train.py --setting=./configs/cv/unet_5.yaml
 
 
-# to run in parallel on multiple GPUs
+# to run in parallel on multiple GPUs (specify which GPU to use in each config file)
 #python ./main_train.py --setting=./configs/cv/unet_1.yaml &
 #sleep 30s
 #python ./main_train.py --setting=./configs/cv/unet_2.yaml &
